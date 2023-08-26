b98dc300961556bae80fb91f46e1659f198dc09c
Iamneo <techadmin@iamneo.ai>
2023-08-26 08:20:10 +0000

    Backup Commit

--- jspservlet-app-bmi-calculator/src/main/java/controller/MainController.java
+++ jspservlet-app-bmi-calculator/src/main/java/controller/MainController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns= {"/jspservelt-app-friends-calculator/friends"})
+@WebServlet(urlPatterns= {"/jspservelt-app-bmi-calculator/main"})
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -25,51 +24,31 @@ public class MainController extends HttpServlet {
 		dispatcher.forward(request, response);
     }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+
+
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
-    	String myName = request.getParameter("friend1");
-    	String friendName = request.getParameter("friend2");
-    	
-        int result = calculate(myName,friendName);
+    	String name = request.getParameter("name");
+		double height = Double.parseDouble(request.getParameter("height"));
+		double weight = Double.parseDouble(request.getParameter("weight"));
 		
-        String calculate = request.getParameter("calculate");
-        
-        System.out.println(result);
-        if(calculate!=null) {
-//        	PrintWriter out = response.getWriter();
-//        	out.println("<h3>Based on the name similarity, the friendship strength between "+myName+" and "+friendName+" is: "+result+"</h3>");
-         	request.setAttribute("myName", myName);
-        	request.setAttribute("friendName", friendName);
-        	request.setAttribute("result", result);
-        }
-        
+		double heightInMeter = height/100;
+	
+		double roundOffBMI  = CalculateBMI.calculate(heightInMeter, weight);
+		
+		request.setAttribute("name", name);
+		request.setAttribute("value",roundOffBMI);
+		
+		String calculate = request.getParameter("calculate");
+		
+		String description = CalculateBMI.description(roundOffBMI);
+		if(calculate != null) {
+			request.setAttribute("description",description);
+		}
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
 		dispatcher.forward(request, response);
 	}
-	
-	//Dont change the above lines of code
-	
-//	Start Enterinng your code here...
-public static int findSum(int num){
-	int sum = 0;
-	while(num>0){
-		sum = sum + (num%10);
-		num = num/10;
-	}
-	return sum;
-}
-public static int calculate(String myName, String friendName) {
-	int f1 = 0,f2 = 0, FRIENDS;
-	for(int i = 0;i<myName.length();i++){
-		f1 += (int)myName.charAt(i);
-	}
-	for(int i =0;i<friendName.length();i++){
-		f2 += (int)friendName.charAt(i);
-	}
-
-	FRIENDS = (f1 + f2) % 100;
-	return FRIENDS;
-}
-	
 }
